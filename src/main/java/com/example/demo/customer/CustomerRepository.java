d94459b623e2e84ba4147f8355be06a04999830c
amigoscode <amigoscode@gmail.com>
2021-02-07 14:58:34 +0000

    section-5

--- src/main/java/com/example/demo/customer/CustomerRepository.java
+++ src/main/java/com/example/demo/customer/CustomerRepository.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Repository;
 import java.util.Collections;
 import java.util.List;
 
-@Repository
 public class CustomerRepository implements CustomerRepo{
     @Override
     public List<Customer> getCustomers() {
