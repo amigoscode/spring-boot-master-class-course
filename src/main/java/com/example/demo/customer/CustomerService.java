098f1ecd9298acc161ac1a114412fa224350b0f6
amigoscode <amigoscode@gmail.com>
2021-02-11 22:03:05 +0000

    section-11

--- src/main/java/com/example/demo/customer/CustomerService.java
+++ src/main/java/com/example/demo/customer/CustomerService.java
@@ -1,6 +1,8 @@
 package com.example.demo.customer;
 
 import com.example.demo.exception.NotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,6 +10,9 @@ import java.util.List;
 
 @Service
 public class CustomerService {
+
+    private final static Logger LOGGER =
+            LoggerFactory.getLogger(CustomerService.class);
 
     private final CustomerRepository customerRepository;
 
@@ -18,6 +23,7 @@ public class CustomerService {
     }
 
     List<Customer> getCustomers() {
+        LOGGER.info("getCustomers was called");
         return customerRepository.findAll();
     }
 
@@ -25,7 +31,11 @@ public class CustomerService {
         return customerRepository
                 .findById(id)
                 .orElseThrow(
-                        () -> new NotFoundException(
-                                "customer with id " + id + " not found"));
+                        () -> {
+                            NotFoundException notFoundException = new NotFoundException(
+                                    "customer with id " + id + " not found");
+                            LOGGER.error("error in getting customer {}", id, notFoundException);
+                            return notFoundException;
+                        });
     }
 }
--- src/main/java/com/example/demo/exception/ApiExceptionHandler.java
+++ src/main/java/com/example/demo/exception/ApiExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.example.demo.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -11,6 +13,9 @@ import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class ApiExceptionHandler {
+
+    private final static Logger LOGGER =
+            LoggerFactory.getLogger(ApiExceptionHandler.class);
 
     @ExceptionHandler(value = ApiRequestException.class)
     public ResponseEntity<Object> handleApiRequestException(
