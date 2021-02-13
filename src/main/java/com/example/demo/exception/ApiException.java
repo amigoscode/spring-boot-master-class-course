536790ac7e496377f6bd7e110b88059a7812ea25
amigoscode <amigoscode@gmail.com>
2021-02-13 01:23:16 +0000

    section-16

--- src/main/java/com/example/demo/exception/ApiException.java
+++ src/main/java/com/example/demo/exception/ApiException.java
@@ -1,14 +1,13 @@
 package com.example.demo.exception;
 
-import lombok.Data;
 import org.springframework.http.HttpStatus;
 
 import java.time.ZonedDateTime;
 
-@Data
-public class ApiException {
-    private final String message;
-    private final Throwable throwable;
-    private final HttpStatus httpStatus;
-    private final ZonedDateTime zonedDateTime;
+// private final, getter, public constructor, equals, hashcode
+// and to string
+public record ApiException (String message,
+                            Throwable throwable,
+                            HttpStatus httpStatus,
+                            ZonedDateTime zonedDateTime) {
 }
